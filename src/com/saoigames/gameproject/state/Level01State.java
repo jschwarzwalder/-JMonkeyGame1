b4bbef39ed78fc760f519a2995fbd64647ebb598
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-06-05 15:28:01 -0700

    following tutorial

--- src/com/saoigames/gameproject/state/Level01State.java
+++ src/com/saoigames/gameproject/state/Level01State.java
@@ -5,10 +5,43 @@
  */
 package com.saoigames.gameproject.state;
 
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetManager;
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.material.Material;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
+
 /**
  *
  * @author SaoiGames
  */
-public class Level01State {
+public class Level01State extends AbstractAppState {
+    private final Node rootNode;
+    private final Node localRootNode = new Node("Level 1");
     
+    public Level01State(SimpleApplication app) {
+        rootNode = app.getRootNode();
+    }
+    
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        
+        rootNode.attachChild(localRootNode);
+    }
+    
+    @Override
+    public void cleanup() {
+        rootNode.detachChild(localRootNode);
+        
+        super.cleanup();
+    }
 }
